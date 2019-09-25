9b7519f612b420cb7416634fb994c10f3d5e1c64
Jamie Markle <jrmarkle@users.noreply.github.com>
2019-09-25 13:58:37 -0400

    copy stuff from protoc-gen-go-template

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jrmarkle/protoc-gen-go-json-enums
+
+require github.com/golang/protobuf v1.3.2
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/jrmarkle/protoc-gen-go-json-enums/generator"
+	"io/ioutil"
+	"os"
+)
+
+type osFileSystem struct{}
+
+func (osFileSystem) ReadFile(filename string) ([]byte, error) { return ioutil.ReadFile(filename) }
+
+func main() {
+	err := generator.NewGenerator(os.Stdin, os.Stdout, osFileSystem{}).Run()
+	if err != nil {
+		os.Stderr.WriteString("Failed: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+}
